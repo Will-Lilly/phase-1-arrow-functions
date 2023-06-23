6c40da56b65ed7187d3caabffa0292c043d94543
Will Lilly <willlilly@Wills-MacBook-Pro-14-M2.local>
2023-06-23 10:39:12 +0800

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+let divide = (a, b) => {
+    let result = a / b;
+    return result;
+}
+
+function square(a) {
+    return a * a;
+}
+
+function square(a) {
+    return a * a;
+}
+
+
+let add = (a, b) => {
+    let result = a + b;
+    return result;
+}
